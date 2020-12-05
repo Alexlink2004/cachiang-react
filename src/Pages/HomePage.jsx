ed6288b94595a41001934d5a7868267cc2a76c10
ALEJANDRO APODACA <alexapodaca@ALEJANDROs-MacBook-Air.local>
2020-12-05 01:50:41 -0800

    New styles

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import Jumbotron from "../Components/Jumbotron.jsx";
 import "./styles/HomePage.css";
 function HomePage() {
+  let lorem ='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+  let loremSM='Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   return (
     <div className="HomePage">
       <Jumbotron
         title="Acerca de nosotros"
-        lead="Somos un grupo de"
-        description="a"
+        lead={loremSM}
+        description={lorem}
         jumboType="jumbo-blue"
+        buttonText="Linkedin"
         
         
         />
@@ -20,15 +23,15 @@ function HomePage() {
         <Jumbotron
         title="Agua Doble Vida Systems"
         lead="Sistema de limpiado de agua"
-        description="fgjnsirnos"
+        description={lorem}
         buttonText="Saber Mas"
         jumboType="jumbo-light-grey"
         
         />
         <Jumbotron
-        title="Agua Doble Vida Systems"
-        lead="Sistema de limpiado de agua"
-        description="fgjnsirnos"
+        title="Otro proyecto perro del adrian "
+        lead="Sin duda estara perro xD"
+        description={lorem}
         buttonText="Saber Mas"
         jumboType="jumbo-dark-grey"
         
